9c980a83897c3e63ba9d83667b3c13bea724d860
Rainbow4795 <asf1234500@outlook.com>
2026-03-14 09:48:53 +0800

    Add files via upload

---
+++ data/system/KeyConfig.js
@@ -0,0 +1,29 @@
+var __tyrano_key_config = {
+	"key": {
+		"13": "next",
+		"32": "hidemessage"
+	},
+	"mouse": {
+		"right": "hidemessage",
+		"center": "menu",
+		"wheel_up": "",
+		"wheel_down": ""
+	},
+	"gesture": {
+		"swipe_up_1": {
+			"action": ""
+		},
+		"swipe_left_1": {
+			"action": ""
+		},
+		"swipe_right_1": {
+			"action": "menu"
+		},
+		"swipe_down_1": {
+			"action": "load"
+		},
+		"hold": {
+			"action": "skip"
+		}
+	}
+}
